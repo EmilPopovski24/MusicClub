ecd17cff63a20cdad25d00351f52fb880b09ea93
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-13 18:48:06 +0300

    update

--- MusicClub/src/app/app.component.ts
+++ MusicClub/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [
+    RouterOutlet, 
+    HomeComponent, 
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
