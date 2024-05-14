1063f57ef306f6b5be654308937f8d86d5f25e13
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-14 20:09:11 +0300

    Update app.routes.ts

--- MusicClub/src/app/app.routes.ts
+++ MusicClub/src/app/app.routes.ts
@@ -1,9 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: "full",
     redirectTo: '/home',
+  },
+  {
+    path: '/login',
+    pathMatch: "full",
+    component: LoginComponent
+  },
+  {
+    path: '/register',
+    pathMatch: "full",
+    component: RegisterComponent
   }
 ];
