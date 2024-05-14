81c2bcc9af4e4b5a0fd1c72b71a6df2eec639c74
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-14 20:11:41 +0300

    update

--- MusicClub/src/app/app.component.ts
+++ MusicClub/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavigationComponent } from './navigation/navigation.component';
