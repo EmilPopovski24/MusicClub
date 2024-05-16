1dec8721cf3305b2a1249f85542ec717f4c3bc62
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-16 15:58:04 +0300

    catalog component implementation

---
+++ MusicClub/src/app/catalog/catalog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog',
+  standalone: true,
+  imports: [],
+  templateUrl: './catalog.component.html',
+  styleUrl: './catalog.component.css'
+})
+export class CatalogComponent {
+
+}
