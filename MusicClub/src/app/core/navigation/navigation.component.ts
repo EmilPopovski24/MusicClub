ce07066f7f911f5709dd71da3344585033986a2e
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-16 14:50:57 +0300

    Update navigation.component.ts

--- MusicClub/src/app/core/navigation/navigation.component.ts
+++ MusicClub/src/app/core/navigation/navigation.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from '../../login/login.component';
+import { RegisterComponent } from '../../register/register.component';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [
+    LoginComponent,
+    RegisterComponent
+  ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
