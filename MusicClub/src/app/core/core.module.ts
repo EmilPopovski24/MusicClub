b6b7fda391796dcb06efe0d414b8fb98e9371892
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-16 15:17:44 +0300

    Update core.module.ts

--- MusicClub/src/app/core/core.module.ts
+++ MusicClub/src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterLink } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavigationComponent, FooterComponent],
   imports: [
     CommonModule, RouterModule, 
   ],
-  exports: []
+  exports: [NavigationComponent, FooterComponent]
 })
 export class CoreModule { }
