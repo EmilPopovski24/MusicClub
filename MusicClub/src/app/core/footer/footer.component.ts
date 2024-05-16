ca98a58cb23d978149a510037dbb5c2f5d7a6620
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-16 16:13:33 +0300

    Update footer.component.ts

--- MusicClub/src/app/core/footer/footer.component.ts
+++ MusicClub/src/app/core/footer/footer.component.ts
@@ -7,6 +7,4 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
