26637b335341a558684cfc0c7ffff195021575bc
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-17 18:59:03 +0300

    Update core.module.ts

--- MusicClub/src/app/core/core.module.ts
+++ MusicClub/src/app/core/core.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, RouterLink } from '@angular/router';
-import { GlobalLoaderComponent } from './global-loader/global-loader.component';
-import { SharedModule } from '../shared/shared.module';
+
 
 @NgModule({
   declarations: [
