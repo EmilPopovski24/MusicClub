e1fc5eedc0cbafd73464fe5f466cc71429417602
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-19 15:13:38 +0300

    fetching data successfully

--- MusicClub/src/app/user/user-list/user-list.component.ts
+++ MusicClub/src/app/user/user-list/user-list.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
 import { UserService } from '../user.service';
+import { User } from '../../types/User';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
 export class UserListComponent implements OnInit {
+  userList:User[] = [];
 
   constructor(private userService: UserService) {}
   
   ngOnInit(): void {
     console.log('ui')
     this.userService.fetchUsers().subscribe((users) => {
-      console.log(users)
+      this.userList = (users)
     })
   }
 }
