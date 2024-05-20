a83c4334075541720c55716422b2ea8f3b28e6d7
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-20 18:58:02 +0300

    Update user.module.ts

--- MusicClub/src/app/user/user.module.ts
+++ MusicClub/src/app/user/user.module.ts
@@ -7,20 +7,20 @@ import { UserListComponent } from './user-list/user-list.component';
 
 
 const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent
-    //canActivate: [AuthActivate]
-  }, 
-  {
-    path: 'register',
-    component: RegisterComponent
-  }, 
-  {
-    path: 'profile',
-    component: ProfileComponent
-    //canActivate: [AuthActivate]
-  },
+  // {
+  //   path: 'login',
+  //   component: LoginComponent
+  //   //canActivate: [AuthActivate]
+  // }, 
+  // {
+  //   path: 'register',
+  //   component: RegisterComponent
+  // }, 
+  // {
+  //   path: 'profile',
+  //   component: ProfileComponent
+  //   //canActivate: [AuthActivate]
+  // },
 ];
 
 @NgModule({
