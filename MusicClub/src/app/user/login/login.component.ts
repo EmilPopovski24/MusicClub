1634c041c5f1a4da6ae2b657489a4e846a90dcf3
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-23 16:46:33 +0300

    update Login and Register forms

--- MusicClub/src/app/user/login/login.component.ts
+++ MusicClub/src/app/user/login/login.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 
 export class LoginComponent {
-
+  
 }
--- MusicClub/src/app/user/register/register.component.ts
+++ MusicClub/src/app/user/register/register.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
