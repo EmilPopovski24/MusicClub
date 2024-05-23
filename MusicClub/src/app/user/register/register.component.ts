523cff1cb8e81781492a4388ebbf6669c299db90
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-23 16:48:35 +0300

    Update register.component.ts

--- MusicClub/src/app/user/register/register.component.ts
+++ MusicClub/src/app/user/register/register.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink, RouterModule],
+  imports: [RouterLink, RouterModule, RouterOutlet],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
