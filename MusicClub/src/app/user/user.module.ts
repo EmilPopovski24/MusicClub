0f624c3b8ec76f61503b646b46d4e4a5b09151ae
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-23 15:52:38 +0300

    update routing for standalone components

--- MusicClub/src/app/user/user.module.ts
+++ MusicClub/src/app/user/user.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterLink, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -7,25 +7,25 @@ import { UserListComponent } from './user-list/user-list.component';
 
 
 const routes: Routes = [
-  // {
-  //   path: 'login',
-  //   component: LoginComponent
-  //   //canActivate: [AuthActivate]
-  // }, 
-  // {
-  //   path: 'register',
-  //   component: RegisterComponent
-  // }, 
-  // {
-  //   path: 'profile',
-  //   component: ProfileComponent
-  //   //canActivate: [AuthActivate]
-  // },
+  {
+    path: 'login',
+    component: LoginComponent
+    //canActivate: [AuthActivate]
+  }, 
+  {
+    path: 'register',
+    component: RegisterComponent
+  }, 
+  {
+    path: 'profile',
+    component: ProfileComponent
+    // canActivate: [AuthActivate]
+  },
 ];
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes), RouterLink],
   exports: [RouterModule]
 })
 
