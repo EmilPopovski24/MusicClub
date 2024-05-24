c364938298a3ab187ddde01664f7f907ff1c473b
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-24 11:07:15 +0300

    Update user.guard.ts

--- MusicClub/src/app/user/user.guard.ts
+++ MusicClub/src/app/user/user.guard.ts
@@ -1,24 +1,24 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { Observable } from "rxjs";
+// import { Injectable } from "@angular/core";
+// import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+// import { Observable } from "rxjs";
 
-@Injectable({ providedIn: 'root'})
+// @Injectable({ providedIn: 'root'})
 
-export class AuthGuard implements CanActivate {
-    constructor(private router:Router) {}
+// export class AuthGuard implements CanActivate {
+//     constructor(private router:Router) {}
 
-    canActivate(
-        route: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot
-    ):
-    | boolean
-    | UrlTree 
-    | Observable<boolean | UrlTree> 
-    | Promise<boolean | UrlTree> {
-        return this.checkIfLogged(state.url)
-    }
-    checkIfLogged(url: string): boolean {
-        return false
-    }
+//     canActivate(
+//         route: ActivatedRouteSnapshot,
+//         state: RouterStateSnapshot
+//     ):
+//     | boolean
+//     | UrlTree 
+//     | Observable<boolean | UrlTree> 
+//     | Promise<boolean | UrlTree> {
+//         return this.checkIfLogged(state.url)
+//     }
+//     checkIfLogged(url: string): boolean {
+//         return false
+//     }
 
-}
+// }
