8ba3e16f2fac74e7301024547c09246b9276d420
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-24 10:39:15 +0300

    update

--- MusicClub/src/app/types/User.ts
+++ MusicClub/src/app/types/User.ts
@@ -1,6 +1,6 @@
 export interface User {
     // id: number;
     email: string;
-    username: string;
-//  password: string
+    // username: string;
+ password: string
 }
--- MusicClub/src/app/user/login/login.component.ts
+++ MusicClub/src/app/user/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { UserService } from '../user.service';
 
 @Component({
