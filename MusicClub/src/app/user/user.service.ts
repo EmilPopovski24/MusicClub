54c3825b796196939a32188afcfc2cfbff2c68d3
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-24 09:58:06 +0300

    Update user.service.ts

--- MusicClub/src/app/user/user.service.ts
+++ MusicClub/src/app/user/user.service.ts
@@ -1,17 +1,18 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
 import { User } from '../types/User';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService implements OnInit {
+export class UserService  {
+  user: User | undefined
 
-  constructor(private http:HttpClient) { }
+  constructor() {
+    try{
 
-  ngOnInit(): void {}
+    } catch(error) {
 
-  fetchUsers() {
-    return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users')
+    }
   }
+  
 }
