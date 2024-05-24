71219c7915e19f921e6df2abe82ff128266589ec
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-24 10:27:24 +0300

    Update user.service.ts

--- MusicClub/src/app/user/user.service.ts
+++ MusicClub/src/app/user/user.service.ts
@@ -1,25 +1,34 @@
 import { Injectable, OnInit } from '@angular/core';
 import { User } from '../types/User';
 
-const USER_KEY = '[user]'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService  {
   user: User | undefined
-
+  USER_KEY = '[user]'
+  
   get isLogged():boolean {
     return !!this.user
   }
 
   constructor() {
     try{
-      const lsUser = localStorage.getItem(USER_KEY) || ""
+      const lsUser = localStorage.getItem(this.USER_KEY) || ""
       this.user = JSON.parse(lsUser)
     } catch(error) {  
       this.user = undefined
     }
+  }
+
+  login(): void {
+
+  }
+
+  logout(): void {
+    this.user
   }
   
 }
