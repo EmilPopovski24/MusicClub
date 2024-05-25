a6ff44294b48942ce3113c8b3279e545b34dddec
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-25 16:24:25 +0300

    update

--- MusicClub/src/environments/environment.development.ts
+++ MusicClub/src/environments/environment.development.ts
@@ -1,7 +1,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: `http://localhost:3000/api`
+  apiUrl: `http://localhost:3000/api`,
+  musicUrl: 'https://musicclub-209c3-default-rtdb.firebaseio.com'
 };
 
 
