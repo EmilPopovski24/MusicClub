0cda97f84f15bdfd166db342550f3a2ebe13644f
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-25 11:59:31 +0300

    Update shared.module.ts

--- MusicClub/src/app/shared/shared.module.ts
+++ MusicClub/src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from './spinner/spinner.component';
+import { AppEmailDirective } from './validators/email.directive';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AppEmailDirective],
   imports: [
     CommonModule, 
-    SpinnerComponent
+    SpinnerComponent,
+    
   ],
   exports: [
-    SpinnerComponent
+    SpinnerComponent,
+    AppEmailDirective
   ]
 })
 export class SharedModule { }
