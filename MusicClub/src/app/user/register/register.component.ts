f2d02b807a6ccf5e2e15225ceb70f9a58ebc28b4
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-25 13:05:51 +0300

    register form ngForm implementation

--- MusicClub/src/app/user/register/register.component.ts
+++ MusicClub/src/app/user/register/register.component.ts
@@ -1,29 +1,35 @@
 import { Component } from '@angular/core';
-import { FormBuilder, NgForm } from '@angular/forms';
+import { FormBuilder, FormsModule, NgForm } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../../shared/shared.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, SharedModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 
 export class RegisterComponent {
 
-  form = this.fb.group({
-    email: ['emailTest'],
-    username: ['usernameTest'],
-    passGroup: this.fb.group({
-      password:["pass"],
-      repassword:["pass"]
-    })
-  })
+  // form = this.fb.group({
+  //   email: ['emailTest'],
+  //   username: ['usernameTest'],
+  //   passGroup: this.fb.group({
+  //     password:["pass"],
+  //     repassword:["pass"]
+  //   })
+  // })
 
-  constructor(private fb:FormBuilder) {}
+  constructor() {}
 
-  register() : void {
-    console.log(this.form.value)
+  register(form:NgForm) : void {
+    if(form.invalid) {
+      return;
+     }
+     
+    console.log(form.value)
   }
 }
