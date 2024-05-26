35090925c070d9acb38b708670b13f731b37f941
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-26 14:21:26 +0300

    apiservice update

--- MusicClub/src/app/api.service.ts
+++ MusicClub/src/app/api.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { Album } from './types/Album';
+import { environment } from '../environments/environment';
+
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ApiService {
+    // musicUrl = environment.musicUrl;
+    // apiUrl = environment.apiUrl;
+    constructor( private http: HttpClient) { }
+  album : Album | undefined ;
 
-  constructor() { }
-}
+    getAlbum(id:string) {
+      const { apiUrl } = environment;
+      return this.http.get<Album>(`${apiUrl}/${id}`);
+    }
+
+    getAlbums() {
+        const musicUrl = environment.musicUrl;
+        return this.http.get<Album[]>(`${musicUrl}/.json`);
+  }
+
+    addAlbum(artist: string,name: string,released: string, genre: string, label: string, singles: string, coverUrl: string) {
+      const musicUrl  = environment.musicUrl
+        return this.http.post<Album[]>(`${musicUrl}/.json`,{artist,name, released, genre, label, singles, coverUrl})
+    }
+  }
--- MusicClub/src/app/catalog/catalog.component.ts
+++ MusicClub/src/app/catalog/catalog.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Album } from '../types/Album';
+import { ApiService } from '../api.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, ],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css'
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit{
+  noAlbums: boolean = false;
+	albumsList: Album[] = [];
+  
+	constructor(private apiService:ApiService) {}
+
+	ngOnInit(): void {
+		
+		this.apiService.getAlbums().subscribe({next:(albumsList) => {
+		this.albumsList = Object.values(albumsList)}})		
+	}
 
 }
