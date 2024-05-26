fe06fa928dbc72d55f9bba68116959dd9dbfc940
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-26 16:23:43 +0300

    Update app.interceptor.ts

--- MusicClub/src/app/app.interceptor.ts
+++ MusicClub/src/app/app.interceptor.ts
@@ -19,7 +19,7 @@ export class AppInterceptor implements HttpInterceptor {
          Observable<HttpEvent<any>> {
             if(req.url.startsWith('/api')) {
                 req = req.clone({
-                    url: req.url.replace(`/api`, `${apiUrl}/users`), 
+                    url: req.url.replace(`/api`, `${apiUrl}`), 
                     withCredentials: true, //for cookie setup
          })} ;
          return next.handle(req).pipe(
