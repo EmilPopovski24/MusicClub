a49d7826984ada39f7107cfbf63b21c5534a35dd
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-27 18:43:12 +0300

    Update app.config.ts

--- MusicClub/src/app/app.config.ts
+++ MusicClub/src/app/app.config.ts
@@ -2,11 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { AppInterceptor } from './app.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [provideRouter(routes), provideHttpClient(withInterceptors([AppInterceptor]))]
 };
 
 // ,provideClientHydration()
