386d56a5b17fa9fd7630bd4512e03e69ad4e257e
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-27 19:57:59 +0300

    Update app.config.ts

--- MusicClub/src/app/app.config.ts
+++ MusicClub/src/app/app.config.ts
@@ -5,7 +5,7 @@ import { routes } from './app.routes';
 import { appInterceptorProvider } from './app.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), [appInterceptorProvider]]
+  providers: [appInterceptorProvider]
 };
 
 
