bc58f074f2cd309c4f6af67d10a2fc045193522b
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-28 18:53:19 +0300

    Update api.service.ts

--- MusicClub/src/app/api.service.ts
+++ MusicClub/src/app/api.service.ts
@@ -19,12 +19,13 @@ export class ApiService {
     }
 
     getAlbums() {
-        const {musicUrl} = environment;
-        return this.http.get<Album[]>(`${musicUrl}/.json`);
+        const { apiUrl } = environment;
+        return this.http.get<Album[]>(`${apiUrl}/.json`);
   }
 
     addAlbum(artist: string,name: string,released: string, genre: string, label: string, singles: string, coverUrl: string) {
-      const musicUrl  = environment.musicUrl
-        return this.http.post<Album[]>(`${musicUrl}/.json`,{artist,name, released, genre, label, singles, coverUrl})
+      const { apiUrl } = environment;
+      // const musicUrl  = environment.musicUrl
+        return this.http.post<Album[]>(`${apiUrl}/.json`,{artist,name, released, genre, label, singles, coverUrl})
     }
   }
