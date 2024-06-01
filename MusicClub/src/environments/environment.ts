5b0f18e9615f116da918d2fdcf7d41de79303dd3
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-06-01 12:24:45 +0300

    Update environment.ts

--- MusicClub/src/environments/environment.ts
+++ MusicClub/src/environments/environment.ts
@@ -1,33 +1,9 @@
 
+
+
 export const environment = {
   production: false,
   apiUrl: `http://localhost:3000/api`,
   musicUrl: 'https://musicclub-209c3-default-rtdb.firebaseio.com'
 };
-
-
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCGXTbgDm69a_3RvmLo6TWhH3NHIpAd99I",
-//   authDomain: "musicclub-209c3.firebaseapp.com",
-//   databaseURL: "https://musicclub-209c3-default-rtdb.firebaseio.com",
-//   projectId: "musicclub-209c3",
-//   storageBucket: "musicclub-209c3.appspot.com",
-//   messagingSenderId: "1006399819951",
-//   appId: "1:1006399819951:web:aa59f52d8fe52addb6c45a",
-//   measurementId: "G-VBJTY8JXC3"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
 
