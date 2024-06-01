9a10865b8a555f1f2edad27587256e61743ae6e9
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-06-01 10:14:11 +0300

    Catalog view

--- MusicClub/src/app/app.routes.ts
+++ MusicClub/src/app/app.routes.ts
@@ -29,7 +29,7 @@ export const routes: Routes = [
         component: CatalogComponent
       },
       {
-        path: ':albumId',
+        path: ':name',
         pathMatch: "full",
         canActivate: [AuthActivate],
         component: DetailsComponent
