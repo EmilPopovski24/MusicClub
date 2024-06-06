5c3bfeffe343c253494669b0e7e30d6c2cc87d62
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-06-06 19:53:25 +0300

    Update login.component.ts

--- MusicClub/src/app/user/login/login.component.ts
+++ MusicClub/src/app/user/login/login.component.ts
@@ -29,37 +29,3 @@ constructor(private userService:UserService, private router:Router) {}
         })
   }  
 }
-
-
-
-// import { Component } from '@angular/core';
-// import { UserService } from '../user.service';
-// import { Router } from '@angular/router';
-// import { FormsModule, NgForm } from '@angular/forms';
-// import { CommonModule } from '@angular/common';
-// import { SharedModule } from '../../shared/shared.module';
-// import { DEFAULT_EMAIL_DOMAINS } from '../../shared/constatnts';
-
-// @Component({
-//   selector: 'app-login',
-//   standalone: true,
-//   imports: [FormsModule, CommonModule, SharedModule],
-//   templateUrl: './login.component.html',
-//   styleUrl: './login.component.css'
-// })
-
-// export class LoginComponent {
-
-//   appEmailDomains = DEFAULT_EMAIL_DOMAINS;
-
-//   constructor(private userService: UserService, private router: Router) {}
-
-//   login(form: NgForm): void {
-//    if(form.invalid) {
-//     return;
-//    }
-    
-//     this.userService.login();
-//     this.router.navigate(['/'])
-//   }
-// }
