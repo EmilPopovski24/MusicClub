ae1b34f9e7996a30bc6b4555f0b40fd77da0ee84
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-07-08 18:18:29 +0300

    Update api.service.ts

--- MusicClub/src/app/api.service.ts
+++ MusicClub/src/app/api.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
 import { Album } from './types/Album';
 import { environment } from '../environments/environment';
 
