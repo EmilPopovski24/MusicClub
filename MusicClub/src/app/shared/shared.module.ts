63880e81660b9b515d1e87d69b885b0fe001b20e
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-07-09 18:29:46 +0300

    setting required labels in the form

--- MusicClub/src/app/shared/shared.module.ts
+++ MusicClub/src/app/shared/shared.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from './spinner/spinner.component';
 import { AppEmailDirective } from './validators/email.directive';
 
-
 @NgModule({
   declarations: [AppEmailDirective],
   imports: [CommonModule, SpinnerComponent],
